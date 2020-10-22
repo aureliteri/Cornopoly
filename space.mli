110057ecb36649048d6d61be5a23e8cab56b8cdc
Meghana Avvaru <meghanaavvaru20@gmail.com>
2020-10-21 23:23:33 -0400

    added stuff

--- space.mli
+++ space.mli
@@ -1,7 +1,15 @@
 
+type property = {
+  description : string;
+  rent_price : float;
+  buy_price : float;
+  color : string
+}
+
+type penalty = {
+  description : string;
+  penalty_price : float
+}
+
 type t
-
-type property
-
-type penalty
 
