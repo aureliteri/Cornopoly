51d6decf3a4097fe9c38bcd63f4d1d0e7c1279b6
aureliteri <amyouyang7@gmail.com>
2020-10-22 23:28:56 -0400

    took iterate out of update_board

--- board.ml
+++ board.ml
@@ -5,20 +5,20 @@ open Space
 let roll_dice =
   let d1 = Random.int 6 +1 in
   let d2 = Random.int 6 +1 in
-  d1+d2
-
+  d1 + d2
 
 let pick_card size =
   let ind = Random.int size in
   Array.get cardlist ind
 
+let rec iterate playerlist (lst: Player.player list) =
+  match playerlist with 
+  | [] -> lst
+  | h :: t -> begin
+      iterate playerlist ((move h roll_dice) :: lst)
+    end
+
 let update_board playerlist =
-  let rec iterate (lst: Player.player list) =
-    match playerlist with 
-    | [] -> lst
-    | h :: t -> begin
-        iterate ((move h roll_dice) :: lst)
-      end
-  in iterate []
+  iterate playerlist []
 
 
