84ce6237dcf378ebdfe457fd01e416e05aa26973
aureliteri <amyouyang7@gmail.com>
2020-10-22 21:59:26 -0400

    implemented card

--- card.ml
+++ card.ml
@@ -1,11 +1,31 @@
-type amount = int 
+type amount = int
 
-type action = 
+type action =
   | Change of amount
-  | Move of amount 
+  | Move of amount
 
 type card =
   {
     description: string;
-    act: action;
+    act: action list;
   }
+
+let card1 = {
+  description = "Pay your Bursar Bill! You owe Cornell $200.";
+  act = [Change ~-200]
+}
+
+let card2 = {
+  description = "Bank pays you dividend of $50.";
+  act = [Change 50]
+}
+
+let card3 = {
+  description = "Advance to Goldwin Smith Hall- If you pass Go, gain $200.";
+  act = [Move 7]
+}
+
+let card4 = {
+  description = "Advance to Go — Collect $200.";
+  act = [Move 1; Change 200]
+}
--- card.mli
+++ card.mli
@@ -3,3 +3,20 @@ type amount
 type action
 
 type card 
+
+(* 1st card *)
+val card1: card
+(* 2nd card *)
+val card2: card
+(* 3rd room *)
+val card3: card
+(* 4th room *)
+val card4: card
+
+val card5: card
+
+val card6: card
+
+val card7: card
+
+val card8: card
