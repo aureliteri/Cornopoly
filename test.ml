ae05c5c8e7e419211e501c8e3d861eb78520fbc4
Aaron Kang <aaronkang612@gmail.com>
2020-10-22 22:00:01 -0400

    test module

---
+++ test.ml
@@ -0,0 +1,20 @@
+open OUnit2
+open Board
+open Player
+open Space
+
+let player_tests =
+  [
+
+  ]
+
+let board_tests = 
+  [
+
+  ]
+
+let suite =
+  "tests for CORNOPOLY" >::: List.flatten [
+    player_tests;
+    board_tests;
+  ]
