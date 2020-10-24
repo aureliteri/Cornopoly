1c4a69d5c351230d688de321e2550601cd54a3d0
Aaron Kang <aaronkang612@gmail.com>
2020-10-23 23:32:43 -0400

    test

--- test.ml
+++ test.ml
@@ -3,30 +3,77 @@ open Board
 open Player
 open Space
 
+(** [property_order list1 list2] compares two lists to see whether
+    they are equivalent lists. list1 and list2 must contain the same elements,
+    though not necessarily in the same order. *)
+let property_order list1 list2 =
+  let uniq1 = List.sort_uniq compare list1 in
+  let uniq2 = List.sort_uniq compare list2 in
+  List.length list1 = List.length uniq1
+  &&
+  List.length list2 = List.length uniq2
+  &&
+  uniq1 = uniq2
+
 let space_list = spacelist
-let player_list= playerlist
+let player_list = playerlist
+let player1 = List.nth playerlist 0
+
+let rec property_name_printer (property_list : Space.property list) (acc : string list) = 
+  match property_list with
+  | [] -> acc
+  | h :: t -> property_name_printer t ((property_name h)::acc)
+
+let current_location_test name player expected_output = 
+  name >:: (fun ctxt -> 
+      assert_equal expected_output (current_location_id player)~printer: string_of_int )
+
+let player_name_test fun_name player expected_output = 
+  fun_name >:: (fun ctxt -> 
+      assert_equal expected_output (name player)~printer: String.escaped)
+
+let player_balance_test name player expected_output = 
+  name >:: (fun ctxt -> 
+      assert_equal expected_output (balance player)~printer: string_of_int)
+
+let player_property_test name player expected_output = 
+  let property_list = property_list player in 
+  let string_property_list = property_name_printer property_list [] in
+  name >:: (fun ctxt -> 
+      assert_equal true 
+        (property_order expected_output (string_property_list)))
+
+let sample_player = sample_player_test
+let move_player_18 = move sample_player 7 
+let move_player_wraparound_1 = move move_player_18 22
+let move_player_wraparound_9 = move move_player_18 30
+let go_player = pass_go sample_player
 
 let player_tests =
   [
-
+    current_location_test "player1 curr loc id" player1 1;
+    player_name_test "player1 name - Meghana" player1 "Meghana";
+    player_balance_test "player1 initial balance" player1 1500;
+    player_property_test "player1 intiial [] property" player1 [];
+    current_location_test "sample player curr loc id" sample_player 11;
+    player_name_test "sample player name - catpotato" sample_player "catpotato";
+    player_balance_test "sample player balance" sample_player 400;
+    player_property_test "sample player property" sample_player ["Donlon"; "Goldwin Smith Hall"; "Collegetown Bagels"];
+    current_location_test "sample player moved 7 to 18" move_player_18 18;
+    current_location_test "move function w/ mod wraparound" move_player_wraparound_1 1;
+    current_location_test "move function w/ mod wraparound" move_player_wraparound_9 9;
+    player_balance_test "sample player balance" go_player 600
   ]
 
-let it_1_player0 = check_space (List.nth space_list 2) (List.nth player_list 0)
-let it_2_player1 = 
+let board_tests = 
+  [
 
-  let check_space_test name space player result =
-    name >:: fun ctxt -> 
-      let play = check_space space player in
-
-
-      let board_tests = 
-        [
-          assert_equal "Test1" (List.nth space_list 2) (List.nth player_list 0) ; 
-        ]
-
+  ]
 
 let suite =
   "tests for CORNOPOLY" >::: List.flatten [
     player_tests;
     board_tests;
   ]
+
+let _ = run_test_tt_main suite
