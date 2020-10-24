dd91e143ddfe72a0f2224f3c79a7bcd7c61796e4
Aaron Kang <aaronkang612@gmail.com>
2020-10-24 01:06:43 -0400

    author init

---
+++ author.ml
@@ -0,0 +1,4 @@
+let aaron_hours_worked = 10
+let michelle_hours_worked = 10
+let amy_hours_worked = 10
+let meghana_hours_worked = 10
---
+++ author.mli
@@ -0,0 +1,12 @@
+(** CS 3110 Fall 2020 Assignment Final Project
+    @author Aaron Kang (uk44), 
+    Michelle Keoy (mtk64), 
+    Amy Ouyang (aco49), 
+    Meghana Avvaru (ma829)
+
+*)
+
+val aaron_hours_worked : int
+val michelle_hours_worked : int
+val amy_hours_worked : int
+val meghana_hours_worked : int
